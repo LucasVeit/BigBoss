409d02b94d24fa16b47d8617f05708ff1fda5d65
Lucas <Lucas-cs1998@hotmail.com>
2021-05-21 15:41:26 -0300

    Adicionado telas para implementação

--- app/src/main/java/com/example/bigboss/telaEditarExercicio.java
+++ app/src/main/java/com/example/bigboss/telaEditarExercicio.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class tela_inicio extends AppCompatActivity {
+public class telaEditarExercicio extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_inicio);
+        setContentView(R.layout.activity_tela_editar_exercicio);
     }
 }
---
+++ app/src/main/java/com/example/bigboss/telaPerguntasFrequentes.java
@@ -0,0 +1,14 @@
+package com.example.bigboss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaPerguntasFrequentes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_perguntas_frequentes);
+    }
+}
