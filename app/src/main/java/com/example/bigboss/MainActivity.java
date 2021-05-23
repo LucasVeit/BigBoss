9fd2e2f19fd2098e8fe4ecaa6a49b21350f8b7f2
Lucas <Lucas-cs1998@hotmail.com>
2021-05-23 15:20:17 -0300

    Adicionado botões para navegar no início

--- app/src/main/java/com/example/bigboss/MainActivity.java
+++ app/src/main/java/com/example/bigboss/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.bigboss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void configuracao(View view){
+        Intent intent = new Intent(this, telaConfiguracao.class);
+        startActivity(intent);
+    }
+
+    public void editarExercicio(View view){
+        Intent intent = new Intent(this, telaEditarExercicio.class);
+        startActivity(intent);
+    }
+
+    public void perguntasFrequentes(View view){
+        Intent intent = new Intent(this, telaPerguntasFrequentes.class);
+        startActivity(intent);
+    }
+
 }
