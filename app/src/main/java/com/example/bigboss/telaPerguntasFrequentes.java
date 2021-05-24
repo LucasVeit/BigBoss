b2085d8006e51981023836006792640e80367c8e
EnergyFall266 <leonardo.vanzin@hotmail.com>
2021-05-23 21:24:07 -0300

    telas configuracao e perguntas frequentes, adicionado icone inicial

--- app/src/main/java/com/example/bigboss/telaPerguntasFrequentes.java
+++ app/src/main/java/com/example/bigboss/telaPerguntasFrequentes.java
@@ -10,5 +10,6 @@ public class telaPerguntasFrequentes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_perguntas_frequentes);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
